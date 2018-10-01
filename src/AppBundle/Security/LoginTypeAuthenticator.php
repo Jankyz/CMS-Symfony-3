2fcb90e23c588f68d7dbcc0fbc17e5b14933ecb2
Geo4you <kontakt@geo4you.waw.pl>
2018-10-01 19:18:51 +0200

    add login Authenticator

---
+++ src/AppBundle/Security/LoginTypeAuthenticator.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace AppBundle\Security;
+
+use AppBundle\Form\LoginType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+
+/**
+ * Class LoginTypeAuthenticator
+ * @package AppBundle\Security
+ */
+class LoginTypeAuthenticator extends AbstractFormLoginAuthenticator
+{
+
+    private $formFactory;
+    private $em;
+    private $router;
+    private $passwordEncoder;
+
+    public function __construct(FormFactoryInterface $formFactory, EntityManagerInterface $em, RouterInterface $router, UserPasswordEncoderInterface $passwordEncoder)
+    {
+        $this->formFactory = $formFactory;
+        $this->em = $em;
+        $this->router = $router;
+        $this->passwordEncoder = $passwordEncoder;
+    }
+
+    protected function getLoginUrl()
+    {
+        return $this->router->generate('login');
+    }
+
+    public function getCredentials(Request $request)
+    {
+        $isLoginSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
+        if (!$isLoginSubmit) {
+            return null;
+        }
+
+        $form = $this->formFactory->create(LoginType::class);
+        $form->handleRequest($request);
+        $data = $form->getData();
+
+        return $data;
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $username = $credentials['username'];
+
+        return $this->em->getRepository('AppBundle:User')
+            ->findOneBy(['username' => $username]);
+
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $password = $credentials['password'];
+
+        if ($this->passwordEncoder->isPasswordValid($user, $password)) {
+            return true;
+        }
+
+        return false;
+    }
+
+    protected function getDefaultSuccessRedirectUrl()
+    {
+        return $this->router->generate('user_list');
+    }
+}
